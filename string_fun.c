cd5101d1322c8aefaa0cdabc05870118bb69be01
Zainab Jimoh <zeecaptcha@zee.local>
2023-05-18 23:59:20 +0100

    Here again today

---
+++ string_fun.c
@@ -0,0 +1,10 @@
+#include "shell.h"
+
+/**
+ * concat_all - concats 3 strings into a newly allocated memory
+ * @name: first string
+ * @sep: second string
+ * @value: third string
+ * Return: pointer to the new concatenated string
+*/
+char *concat_all(char *name, char *sep, char *value);
