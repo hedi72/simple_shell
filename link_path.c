1afbaa629942a252a404ba9128928f4f3aba1824
Zainab Jimoh <zeecaptchat@gmail.com>
2023-05-19 14:47:24 +0100

    Betty didn't like me so Miss Zee fixed that

--- link_path.c
+++ link_path.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+
 /**
  * add_node - add a new node at the end
  * @head: pointer to pointer
@@ -7,28 +8,28 @@
  */
 list_path *add_node(list_path **head, char *str)
 {
-  if (!head || !str)
-    return (NULL)
-      list_path *new = malloc(sizeof(list_path));
-  if (!new)
-    return (NULL);
-  new->dir = str;
-  new->p = NULL;
+if (!head || !str)
+return (NULL);
+list_path *new = malloc(sizeof(list_path));
+if (!new)
+return (NULL);
+new->dir = str;
+new->p = NULL;
 
-  if (!*head)
-    {
-      *head = new;
-    }
-  else
-    {
-      list_path *last = *head;
-      while (last->p)
-	{
-	  last = last->p;
-	}
-      last->p new;
-    }
-  return (*head);
+if (!*head)
+{
+*head = new;
+}
+else
+{
+list_path *last = *head;
+while (last->p)
+{
+last = last->p;
+}
+last->p = new;
+}
+return (*head);
 }
 
 /**
@@ -36,67 +37,66 @@ list_path *add_node(list_path **head, char *str)
  * @name: name of the variable
  * Return: string of value
  */
-
 char *get_env(const char *name)
 {
-  if (!name)
-    return (NULL);
-  size_t name_len = 0;
-  const char *env_var;
-  while ((env_var = environ[name_len]) != NULL)
-    {
-      size_t i = 0;
-      while (name[i] && name[i] == env_var[i])
-	i++;
-      if (name[i] == '\0' && env_var[i] == '=')
-	return ((char *)(env_var + i + 1);
-		name_len++;
-		}
-	  return (NULL);
-    }
+if (!name)
+return (NULL);
+size_t name_len = 0;
+const char *env_var;
+while ((env_var = environ[name_len]) != NULL)
+{
+size_t i = 0;
+while (name[i] && name[i] == env_var[i])
+i++;
+if (name[i] == '\0' && env_var[i] == '=')
+return ((char *)(env_var + i + 1));
+name_len++;
+}
+return (NULL);
+}
 
 
-  /**
-   * linkpath - create a linked listfor path
-   * @path: string of path
-   * Return: pointer to the created link
-   */
-  list_path *linkpath(char *path)
-  {
-    list_path *head = '\0';
-    char *token;
-    char *cpath = _strdup(path);
-    token = strtok(cpath, ":");
-    while (token)
-      {
-	head = add_node(&head, token);
-	token = strtok(NULL, ":");
-      }
-    return (head);
-  }
+/**
+* linkpath - create a linked listfor path
+* @path: string of path
+* Return: pointer to the created link
+*/
+list_path *linkpath(char *path)
+{
+list_path *head = '\0';
+char *token;
+char *cpath = _strdup(path);
+token = strtok(cpath, ":");
+while (token)
+{
+head = add_node(&head, token);
+token = strtok(NULL, ":");
+}
+return (head);
+}
 
-  /**
-   * find_path - finds the pathname of a file
-   * @filename: name of file
-   * @head: head of linked list
-   * Return: pathname of filename
-   */
-  char *find_path(char *filename, list_path *head)
-  {
-    struct stat st;
-    char path[MAX_PATH_LENGTH];
-    list_path *tmp = head;
-    while (tmp)
-      {
-	snprintf(path, sizeof(path), "%s/%s", tmp->dir, filename);
-	if (stat(path, &st) == 0)
-	  {
-	    return (strdup(path));
-	  }
-	tmp = tmp->p;
-      }
-    return (NULL);
-  }
+/**
+* find_path - finds the pathname of a file
+* @filename: name of file
+* @head: head of linked list
+* Return: pathname of filename
+*/
+char *find_path(char *filename, list_path *head)
+{
+struct stat st;
+char path[MAX_PATH_LENGTH];
+list_path *tmp = head;
+while (tmp)
+{
+snprintf(path, sizeof(path), "%s/%s", tmp->dir, filename);
+if (stat(path, &st) == 0)
+{
+return (strdup(path));
+}
+tmp = tmp->p;
+}
+return (NULL);
+}
 
 /**
  * free_list - free a list_
