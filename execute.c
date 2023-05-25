e9daff6717c60e2b252857e65e3d493b8231de9c
Zainab Jimoh <zeecaptchat@gmail.com>
2023-05-25 20:49:03 +0100

    fixed comments

--- execute.c
+++ execute.c
@@ -1,13 +1,12 @@
 #include "shell.h"
+
 /**
- * command_executer - Executes commands given.
- *
+ * command_executer - Executes commands given by user.
  * @path: The path of the command to execute.
  * @av: An array of strings containing the command and its arguments.
  * @env: An array of strings representing the environment variables.
  * @status: A pointer to an integer variable to set the status.
- *
- * Returns: NOTHING.
+ * Returns: Nothing.
  */
 void command_executer(char *path, char **av, char **env, int *status)
 {
