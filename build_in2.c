3f0c2613a921d866460860078deac8e90a2fa553
hedi72 <heditaieb72@gmail.com>
2023-05-25 08:32:58 -0700

    f

--- build_in2.c
+++ build_in2.c
@@ -26,7 +26,7 @@ char *_getenvi(const char *name)
 		}
 	}
 
-	return value;
+	return (value);
 }
 
 /**
@@ -127,31 +127,5 @@ char *command_array, int type_of_error)
 	free(counter_string);
 }
 
-/**
- * process_token - Processes a token and returns the corresponding string.
- * @token: The input token to process.
- * @status: An integer representing the status of the command.
- * Return: A dynamically allocated string representing the processed token.
- */
-char *process_token(char *token, int status)
-{
-	char *cmde, *variable;
-
-	cmde = NULL;
-	variable = NULL;
-
-	if (_strcmp("$$", token) == 0)
-		cmde = get_processid();
-	else if (_strcmp("$?", token) == 0)
-		cmde = get__status(status);
-	else if (token[0] == '$' && token[1] != '\0')
-	{
-		variable = _getenvi(&token[1]);
-		cmde = (variable != NULL) ? _strdup(variable) : _strdup("");
-	}
-	else
-		cmde = _strdup(token);
-	return (cmde);
-}
 
 
