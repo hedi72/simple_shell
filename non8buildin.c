9519ed2197d9393d7bd54bafb5d4afff74eefebb
hedi <113123576+hedi72@users.noreply.github.com>
2023-05-25 16:26:40 +0100

    Update non8buildin.c

--- non8buildin.c
+++ non8buildin.c
@@ -1,7 +1,7 @@
 #include"shell.h"
 
 /**
-* nonbuiltin_hondler - Handle non-builtin commands
+* nonbuiltin_hndler - Handle non-builtin commands
 * @command_array: Array of command arguments
 * @env: Environment variables
 * @status: Pointer to the status variable
@@ -11,25 +11,25 @@
 * This function handles non-builtin commands in the shell.
 * by checking the access permission.
 */
-void nonbuiltin_hondler(char **command_array, char *env[], int *status,
+void nonbuiltin_hndler(char **command_array, char *env[], int *status,
 						int count, list_paths *current, char *argv[])
 {
 	char *new_path;
 
 	if (access(command_array[0], X_OK) == 0)
-		execute_command(command_array[0], command_array, env, status);
+		command_executer(command_array[0], command_array, env, status);
 	else
 	{
 		/*if the user just entered the command without the whole path*/
-		new_path = check_acces(command_array[0], current);
+		new_path = check_access(command_array[0], current);
 		if (new_path)
 		{
-			execute_command(new_path, command_array, env, status);
+			command_executer(new_path, command_array, env, status);
 			free(new_path);
 		}
 		else
 		{
-			print_err(argv[0], count, command_array[0], NOT_FOUND);/*----lsa----*/
+			print_error(argv[0], count, command_array[0], NOT_FOUND);/*----lsa----*/
 			*status = NOT_FOUND;
 		}
 	}
