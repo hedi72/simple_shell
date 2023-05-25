8b782011a3d39f7647723c0056a43c85dd3ef205
hedi <113123576+hedi72@users.noreply.github.com>
2023-05-25 16:49:06 +0100

    Update string_fun3.c

--- string_fun3.c
+++ string_fun3.c
@@ -1,174 +1,48 @@
-#include "shell.h"
+#include"shell.h"
 /**
- * _atoi - Converts a string to an integer.
- * @s: The string to be converted, input by user
+ * number_to_character - Converts an integer to a string.
+ * @number: The integer to be converted.
  *
- * Return: The converted integer,
- * or -1 on conversion error.
+ * Return: The string representation of the integer,
+ * or NULL on failure.
  */
-int _atoi(char *s)
+
+char *number_to_character(int number)
 {
-	unsigned int n, i;
-	char positive;
+	int count = 0, temprory = number;
+	char *copied_number;
 
-	i = 0;
-	n = 0;
-
-	/* Iterate through each character of the string */
-	while (s[i] != '\0')
+	/*Count the digits in the input number*/
+	if (number == 0)
+		count = 1;
+	else
 	{
-		/* Check if the character is a non-digit */
-		if (!((s[i] >= '0') && (s[i] <= '9')))
+		while (temprory != 0)
 		{
-			return (-1);
-		}
-
-		/* Check if the character is a digit */
-		if (((s[i] >= '0') && (s[i] <= '9')))
-		{
-			n = (n * 10) + (s[i] - '0');
-		}
-		else if (s[i] == '+')
-		{
-			positive++;
-		}
-
-		i++;
-	}
-
-	/* Return the converted integer */
-	return (n);
-}
-
-/**
- * text_to_array - converts text to an array of strings
- *
- * @text_read: the text to convert
- *
- * Return: a pointer to a char array containing the converted text
- */
-char **text_to_array(char *text_read)
-{
-	char *token, *command;
-	char **command_lines;
-	int i;
-	unsigned int characters_count;
-
-	characters_count = 0;
-	command_lines = NULL;
-	i = 0;
-	characters_count = piped_characters_count(text_read, '\n');
-	command_lines = (char **)malloc((characters_count + 1) * sizeof(char *));
-	token = strtok(text_read, "\n");
-	command = _strdup(token);
-	command_lines[i++] = command;
-	while (token != NULL)
-	{
-		token = _strtok(NULL, "\n");
-		if (token != NULL)
-		{
-			command = _strdup(token);
-			command_lines[i++] = command;
+			temprory = temprory / 10;
+			count++;
 		}
 	}
-	free(text_read);
-	command_lines[i] = NULL;
-	return (command_lines);
-}
-/**
- * int_to_str - Converts an integer to a string.
- * @n: The integer to convert.
- * @str: The buffer to store the resulting string.
- * Return : nothing
- */
-void int_to_str(int n, char *str)
-{
-	int tmp;
-	int i = 0;
 
-	if (n == 0)
+	/* Allocate memory for the character array */
+	copied_number = malloc(sizeof(char) * (count + 1));
+	if (!copied_number)
 	{
-		str[i++] = '0';
-	}
-	tmp = n;
-	while (tmp > 0)
-	{
-		tmp /= 10;
-		i++; 
-	}
-	tmp = n;
-	while (i > 0)
-	{
-		str[--i] = tmp % 10 + '0'; 
-		tmp /= 10; 
-
-	}
-	str[i] = '\0'; 
-
-}
-/**
- * line_count - Counts the number of lines in a given string.
- * @line: The input string.
- * Return: The number of lines in the string.
- */
-int line_count(char *line)
-{
-	int i = 0;
-
-	while (*line != '\0')
-	{
-		i++;
-		line++;
-	}
-	return (i);
-}
-
-/**
- * line_vector - converts a string into an array of strings
- * @command: the string to convert
- * @status: an integer representing the status of the command
- * Return: a pointer to a char array.
- */
-char **line_vector(char *command, int status)
-{
-	char *copied_line, *token, **argument_vector, *variable, *cmde;
-	int i = 0, character_count;
-
-	
-	if (command[0] == '\0')
+		perror("malloc error");
 		return (NULL);
-	copied_line = _strdup(command);
-	if (copied_line == NULL)
-		return (NULL); /*can't cpy*/
-	character_count = char_counter(copied_line, ' ');
-	argument_vector = malloc((character_count + 1) * sizeof(char *));
-	token = _strtok(copied_line, TOK_D);
-
-	cmde = flags_handler(token, command, argument_vector, status);
-	argument_vector[i++] = cmde;
-	while (token != NULL)
-	{
-		token = _strtok(NULL, TOK_D);
-		if (token != NULL)
-		{
-			if (_strcmp("$$", token) == 0)
-				cmde = get_processid();
-			else if (_strcmp("$?", token) == 0)
-				cmde = get__status(status);
-			else if ((token[0] == '$') && (token[1]))
-			{
-				variable = _getenvi(&token[1]);
-				if (variable)
-					cmde = _strdup(variable);
-				else
-					cmde = _strdup("");
-			}
-			else
-				cmde = _strdup(token);
-			argument_vector[i++] = cmde;
-		}
 	}
-	argument_vector[i] = NULL;
-	free(copied_line);
-	return (argument_vector);
+
+	/* Set the null terminator at the end of the array */
+	copied_number[count] = '\0';
+
+	/* Convert each digit of the integer to a character and store in the array */
+	while (count != 0)
+	{
+		count--;
+		copied_number[count] = '0' + number % 10;
+		number = (number / 10);
+	}
+
+	/* Return the converted string */
+	return (copied_number);
 }
