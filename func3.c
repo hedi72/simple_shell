c3a5f392bf339c35faa0594389f40a8f6d704a63
hedi72 <heditaieb72@gmail.com>
2023-05-26 02:20:02 -0700

    ff

--- func3.c
+++ func3.c
@@ -48,29 +48,23 @@ int _atoi(char *s)
 
 char *_getenv(const char *name)
 {
-	int i;
-	char *variable_name;
-	char *variable_value;
-	char *value = NULL;
+int i;
+char *variable_name;
+char *variable_value;
+char *value = NULL;
 
-	
-	for (i = 0; environ[i] != NULL; i++)
-	{
-		
-		variable_name = strtok(environ[i], "=");
-		
-		variable_value = strtok(NULL, "=");
+for (i = 0; environ[i] != NULL; i++)
+{
+variable_name = strtok(environ[i], "=");
+variable_value = strtok(NULL, "=");
+if (strcmp(variable_name, name) == 0)
+{
+value = variable_value;
+break;
+}
+}
 
-
-		if (strcmp(variable_name, name) == 0)
-		{
-		
-			value = variable_value;
-			break;
-		}
-	}
-
-	return (value);
+return (value);
 }
 
 /**
@@ -83,4 +77,40 @@ char *_getenv(const char *name)
 char *_strtok(char *str, const char *delimiters)
 {
 	return (strtok(str, delimiters));
+}
+
+/**
+ * text_to_array - converts text to an array of strings
+ *
+ * @text_read: the text to convert
+ *
+ * Return: a pointer to a char array containing the converted text
+ */
+char **text_to_array(char *text_read)
+{
+	char *token, *command;
+	char **command_lines;
+	int i;
+	unsigned int characters_count;
+
+	characters_count = 0;
+	command_lines = NULL;
+	i = 0;
+	characters_count = piped_characters_count(text_read, '\n');
+	command_lines = (char **)malloc((characters_count + 1) * sizeof(char *));
+	token = strtok(text_read, "\n");
+	command = _strdup(token);
+	command_lines[i++] = command;
+	while (token != NULL)
+	{
+		token = _strtok(NULL, "\n");
+		if (token != NULL)
+		{
+			command = _strdup(token);
+			command_lines[i++] = command;
+		}
+	}
+	free(text_read);
+	command_lines[i] = NULL;
+	return (command_lines);
 }
